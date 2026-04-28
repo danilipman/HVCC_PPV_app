4079e8bfdb121edf9e9fd621c72be2cb600993ce
danilipman <148462170+danilipman@users.noreply.github.com>
2026-04-28 14:29:36 -0500

    add data folder

---
+++ DATA/ellipse_util.py
@@ -0,0 +1,91 @@
+from numpy import linspace, array, logspace, sin, cos, pi, arange, sqrt, arctan2, arccos
+
+
+
+
+
+
+
+### ELLIPSE ###
+def rotate(x,y,theta):
+    xprime = x*cos(theta) - y*sin(theta)
+    yprime = x*sin(theta) + y*cos(theta)
+    return xprime, yprime
+
+
+class Ring:
+
+    def __init__(self,t,a,b,z,v0,theta,alpha,xyzsun,vxyzsun):
+        self.t     = t
+        self.a     = a
+        self.b     = b
+        self.z0    = z
+        self.v0    = v0
+        self.theta = theta
+        self.x     = a*cos(t)
+        self.y     = -b*sin(t)
+        self.z     = self.z0*sin(-2*t + alpha)
+        self.R     = sqrt(self.x**2+self.y**2)
+        self.phi   = -arctan2(self.y,self.x)
+        self.ephix = -sin(self.phi) # unit vector parallel to circle
+        self.ephiy = -cos(self.phi) # unit vector parallel to circle
+        norm       = sqrt((a*sin(t))**2+(b*cos(t))**2)
+        self.ex    = -a*sin(t)/norm # unit vector parellel to ellipse
+        self.ey    = -b*cos(t)/norm  # unit vector parallel to ellipse
+        self.cosalpha = self.ex*self.ephix + self.ey*self.ephiy
+        self.vphi  = self.R[0]*self.v0/self.R # assume conservation of angular momentum
+        self.v     = self.vphi/self.cosalpha # total speed along the orbit
+        self.vx    = +self.v*self.ex
+        self.vy    = +self.v*self.ey
+        self.vz    = np.zeros(t.size)
+        self.x,self.y   = rotate(self.x,self.y,theta)
+        self.vx,self.vy = rotate(self.vx,self.vy,theta)
+        self.X,self.Y,self.Z,self.vX,self.Vy,self.vZ = xyz2XYZ(self.x,self.y,self.z,self.vx,self.vy,self.vz,xyzsun[0],xyzsun[1],xyzsun[2],vxyzsun[0],vxyzsun[1],vxyzsun[2])
+        # Convert Sgr A* offset from degrees to radians
+        l_offset = np.radians(0.05)
+        b_offset = np.radians(-0.0462)
+        x_offset = Rsun*l_offset
+        y_offset = 0
+        z_offset = Rsun*b_offset
+        # Calculate galactic coordinates accounting for Sgr A* offset
+        self.l,self.b,self.r,self.vl,self.vb,self.vr = xyz2lbr(
+            self.x,self.y,self.z,self.vx,self.vy,self.vz,
+            xyzsun[0],xyzsun[1],xyzsun[2],vxyzsun[0],vxyzsun[1],vxyzsun[2])
+        self.l += l_offset
+        self.b += b_offset
+        self.x += x_offset
+        self.y += y_offset
+        self.z += z_offset
+        self.mu_l, self.mu_b = vlb_2_mulb(self.r, self.vl*100, self.vb*100)
+        self.mu_l, self.mu_b = vlb_2_mulb(self.r,self.vl*100,self.vb*100)
+
+
+#############################
+# define sun position & velocity
+#############################
+
+xsun  = 0.0
+ysun  = -8.2
+zsun  = 0.0
+vxsun = -2.2
+vysun = 0.0
+vzsun = 0.0
+xyzsun  = [xsun, ysun, zsun ]
+vxyzsun = [vxsun,vysun,vzsun]
+phisun  = arctan2(ysun,xsun)
+
+#############################
+# define rings using median model for Lipman+2026
+#############################
+
+a_rings = 0.083 # x axis of the rings in kpc
+b_rings = 0.034 # y axis of the rings in kpc
+z_rings = 0.0140 # height of the ring above/below plane in kpc
+v_rings = 129.6 # tangential velocity at initial point
+theta_rings = np.radians(16.6) # inclination of the rings
+alpha_rings = 0.12
+dphi = np.radians(1)
+phi  = arange(phisun+dphi/2,phisun+2*pi,dphi)
+Rings = Ring(phi,a_rings,b_rings,z_rings,v_rings,theta_rings,alpha_rings,xyzsun,vxyzsun)
+
+
